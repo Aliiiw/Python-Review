76a2ea331df18fcf086bca313e45e1b74e215035
Ali Rahimi <ali.rahimi@gandom.co>
2026-05-29 20:47:17 +0200

    list

--- main.py
+++ main.py
@@ -1,75 +1,150 @@
-# x = 15
-# y = 4
+# thislist = ["apple", "banana", "cherry", "apple", "cherry"]
+# print(thislist)
 
-# print(x + y)
-# print(x - y)
-# print(x * y)
-# print(x / y)
-# print(x % y)
-# print(x**y)
-# print(x // y)
-
-# x = 12
-# y = 5
-
-# print(x / y)
+# print(len(thislist))
 
 
-# x = 12
-# y = 5
+# list1 = ["apple", "banana", "cherry"]
+# list2 = [1, 5, 7, 9, 3]
+# list3 = [True, False, False]
 
-# print(x // y)
-
-
-# numbers = [1, 2, 3, 4, 5]
-
-# if (count := len(numbers)) > 3:
-#     print(f"List has {count} elements")
+# list1 = ["abc", 34, True, 40, "male"]
 
 
-# num = 6
+# mylist = ["apple", "banana", "cherry"]
+# print(type(mylist))
 
-# x = "WEEKEND!" if num > 5 else "Workday"
+# thislist = ["apple", "banana", "cherry"]
+# print(thislist[1])
+# print(thislist[-1])
+# thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+# print(thislist[2:5])
 
-# print(x)
-
-
-# x = 5
-# y = 3
-
-# print(x == y)
-# print(x != y)
-# print(x > y)
-# print(x < y)
-# print(x >= y)
-# print(x <= y)
-
-# x = 5
-
-# print(1 < x < 10)
-
-# print(1 < x and x < 10)
-
-# x = ["apple", "banana"]
-# y = ["apple", "banana"]
-# z = x
-
-# print(x is z)
-# print(x is y)
-# print(x == y)
-
-# fruits = ["apple", "banana", "cherry"]
-
-# print("banana" in fruits)
-
-# text = "Hello World"
-
-# print("H" in text)
-# print("hello" in text)
-# print("z" not in text)
+# thislist = ["apple", "banana", "cherry"]
+# if "apple" in thislist:
+#     print("Yes, 'apple' is in the fruits list")
 
 
-# print(6 & 3)
-# print(6 | 3)
+# thislist = ["apple", "banana", "cherry"]
+# thislist[1] = "blackcurrant"
+# print(thislist)
 
-print(5 + 4 - 7 + 3)
+# thislist = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
+# thislist[1:3] = ["blackcurrant", "watermelon"]
+# print(thislist)
+
+# thislist = ["apple", "banana", "cherry"]
+# thislist.insert(2, "watermelon")
+# print(thislist)
+
+# thislist = ["apple", "banana", "cherry"]
+# thislist.append("orange")
+# print(thislist)
+
+# thislist = ["apple", "banana", "cherry"]
+# tropical = ["mango", "pineapple", "papaya"]
+# thislist.extend(tropical)
+# print(thislist)
+
+# thislist = ["apple", "banana", "cherry"]
+# thislist.remove("banana")
+# print(thislist)
+
+# thislist = ["apple", "banana", "cherry"]
+# thislist.pop(1)
+# print(thislist)
+
+# thislist = ["apple", "banana", "cherry"]
+# del thislist[0]
+# print(thislist)
+
+# thislist = ["apple", "banana", "cherry"]
+# thislist.clear()
+# print(thislist)
+
+# thislist = ["apple", "banana", "cherry"]
+# for x in thislist:
+#     print(x)
+
+# thislist = ["apple", "banana", "cherry"]
+# for i in range(len(thislist)):
+#     print(thislist[i])
+
+# thislist = ["apple", "banana", "cherry"]
+# i = 0
+# while i < len(thislist):
+#     print(thislist[i])
+#     i = i + 1
+
+
+# thislist = ["apple", "banana", "cherry"]
+# [print(x) for x in thislist]
+
+# fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+# newlist = []
+
+# for x in fruits:
+#     if "a" in x:
+#         newlist.append(x)
+
+# print(newlist)
+
+
+# fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+
+# newlist = [x for x in fruits if "a" in x]
+# print(newlist)
+
+# newlist = [expression for item in iterable if condition == True]
+
+# thislist = ["orange", "mango", "kiwi", "pineapple", "banana"]
+# thislist.sort()
+# print(thislist)
+
+# thislist = [100, 50, 65, 82, 23]
+# thislist.sort()
+# print(thislist)
+# thislist = ["orange", "mango", "kiwi", "pineapple", "banana"]
+# thislist.sort(reverse=True)
+# print(thislist)
+
+
+# def myfunc(n):
+#     return abs(n - 50)
+
+
+# thislist = [100, 50, 65, 82, 23]
+# thislist.sort(key=myfunc)
+# print(thislist)
+
+
+# thislist = ["banana", "Orange", "Kiwi", "cherry"]
+# thislist.reverse()
+# print(thislist)
+
+
+# thislist = ["apple", "banana", "cherry"]
+# mylist = thislist.copy()
+# print(mylist)
+
+# thislist = ["apple", "banana", "cherry"]
+# mylist = list(thislist)
+# print(mylist)
+
+# thislist = ["apple", "banana", "cherry"]
+# mylist = thislist[:]
+# print(mylist)
+
+# list1 = ["a", "b", "c"]
+# list2 = [1, 2, 3]
+
+# list3 = list1 + list2
+# print(list3)
+
+# list1 = ["a", "b", "c"]
+# list2 = [1, 2, 3]
+
+# for x in list2:
+#     list1.append(x)
+
+# print(list1)
