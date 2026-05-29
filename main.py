f81e55f2c7ea4e15600d4fc632a6b76fdf8198f5
Ali Rahimi <ali.rahimi@gandom.co>
2026-05-29 14:25:10 +0200

    recap simple thing comments

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
-x = 5
+print("Hello World!")
+print("I am learning Python.")
+print("It is awesome!")
 
-y = 6
+print("This will work!")
+print("This will also work!")
 
-print(x + y)
+print("this is the text in line", end=" ")
+print("this is the text nexto line")
+
+
+print(3)
+print(50)
+
+print(3 + 3)
+print(2 * 4)
+
+print("I'm", 24, "Years Old!")
+print("I'm" + " 24 " + "Years Old!")
