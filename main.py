c552dc0f4ee87773e955a29d431c94d8f28fe9b6
Ali Rahimi <ali.rahimi@gandom.co>
2026-05-29 17:19:55 +0200

    string

--- main.py
+++ main.py
@@ -1,42 +1,88 @@
-x = 1j
+print("It's alright")
+print("He is called 'Johnny'")
+print('He is called "Johnny"')
 
 
-print(type(x))
-
-
-x = 35e3
-y = 12e4
-z = -87.7e100
-
-print(type(x))
-print(type(y))
-print(type(z))
-
-x = 3 + 5j
-y = 5j
-z = -5j
-
-print(type(x))
-print(type(y))
-print(type(z))
-
-x = 1  # int
-y = 2.8  # float
-z = 1j  # complex
-
-# convert from int to float:
-a = float(x)
-
-# convert from float to int:
-b = int(y)
-
-# convert from int to complex:
-c = complex(x)
-
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
 print(a)
-print(b)
-print(c)
 
-print(type(a))
-print(type(b))
-print(type(c))
+
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a)
+
+
+a = "Hello, World!"
+print(a[1])
+
+for x in "banana":
+    print(x)
+
+
+a = "Hello, World!"
+print(len(a))
+
+txt = "The best things in life are free!"
+print("free" in txt)
+
+txt = "The best things in life are free!"
+if "free" in txt:
+    print("Yes, 'free' is present.")
+
+txt = "The best things in life are free!"
+print("expensive" not in txt)
+
+
+b = "Hello, World!"
+print(b[2:5])
+
+
+b = "Hello, World!"
+print(b[:5])  # 5 not included
+
+b = "Hello, World!"
+print(b[2:])
+
+
+b = "Hello, World!"
+print(b[-5:-2])
+
+a = "Hello, World!"
+print(a.upper())
+
+a = "Hello, World!"
+print(a.lower())
+
+a = " Hello, World! "
+print(a.strip())  # returns "Hello, World!"
+
+a = "Hello, World!"
+print(a.replace("H", "J"))
+
+a = "Hello, World!"
+print(a.split(","))  # returns ['Hello', ' World!']
+
+age = 36
+# This will produce an error:
+txt = f"My name is John, I am {age}"
+print(txt)
+
+price = 59
+txt = f"The price is {price} dollars"
+print(txt)
+
+price = 59
+txt = f"The price is {price:.2f} dollars"
+print(txt)
+
+txt = f"The price is {20 * 59} dollars"
+print(txt)
+
+
+txt = 'We are the so-called "Vikings" from the north.'
+print(txt)
