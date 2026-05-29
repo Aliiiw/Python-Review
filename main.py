efba0f52f358c69a68d18ac354ef4501fb84c665
Ali Rahimi <ali.rahimi@gandom.co>
2026-05-29 22:03:23 +0200

    tuples

--- main.py
+++ main.py
@@ -1,150 +1,82 @@
-# thislist = ["apple", "banana", "cherry", "apple", "cherry"]
-# print(thislist)
-
-# print(len(thislist))
+# thistuple = ("apple", "banana", "cherry", "apple", "cherry")
+# print(thistuple)
 
 
-# list1 = ["apple", "banana", "cherry"]
-# list2 = [1, 5, 7, 9, 3]
-# list3 = [True, False, False]
+# thistuple = ("apple", "banana", "cherry")
+# print(len(thistuple))
 
-# list1 = ["abc", 34, True, 40, "male"]
+# thistuple = ("apple",)
+# print(type(thistuple))
 
-
-# mylist = ["apple", "banana", "cherry"]
-# print(type(mylist))
-
-# thislist = ["apple", "banana", "cherry"]
-# print(thislist[1])
-# print(thislist[-1])
-# thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
-# print(thislist[2:5])
-
-# thislist = ["apple", "banana", "cherry"]
-# if "apple" in thislist:
-#     print("Yes, 'apple' is in the fruits list")
+# # NOT a tuple
+# thistuple = "apple"
+# print(type(thistuple))
 
 
-# thislist = ["apple", "banana", "cherry"]
-# thislist[1] = "blackcurrant"
-# print(thislist)
+# tuple1 = ("apple", "banana", "cherry")
+# tuple2 = (1, 5, 7, 9, 3)
+# tuple3 = (True, False, False)
 
-# thislist = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
-# thislist[1:3] = ["blackcurrant", "watermelon"]
-# print(thislist)
+# tuple1 = ("abc", 34, True, 40, "male")
 
-# thislist = ["apple", "banana", "cherry"]
-# thislist.insert(2, "watermelon")
-# print(thislist)
+# print(tuple1)
 
-# thislist = ["apple", "banana", "cherry"]
-# thislist.append("orange")
-# print(thislist)
+# thistuple = ("apple", "banana", "cherry")
+# print(thistuple[1])
 
-# thislist = ["apple", "banana", "cherry"]
-# tropical = ["mango", "pineapple", "papaya"]
-# thislist.extend(tropical)
-# print(thislist)
+# thistuple = ("apple", "banana", "cherry")
+# print(thistuple[-1])
 
-# thislist = ["apple", "banana", "cherry"]
-# thislist.remove("banana")
-# print(thislist)
+# thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+# print(thistuple[2:5])
 
-# thislist = ["apple", "banana", "cherry"]
-# thislist.pop(1)
-# print(thislist)
+# x = ("apple", "banana", "cherry")
+# y = list(x)
+# y[1] = "kiwi"
+# x = tuple(y)
 
-# thislist = ["apple", "banana", "cherry"]
-# del thislist[0]
-# print(thislist)
+# print(x)
 
-# thislist = ["apple", "banana", "cherry"]
-# thislist.clear()
-# print(thislist)
+# thistuple = ("apple", "banana", "cherry")
+# y = list(thistuple)
+# y.remove("apple")
+# thistuple = tuple(y)
+# print(thistuple)
 
-# thislist = ["apple", "banana", "cherry"]
-# for x in thislist:
-#     print(x)
+# fruits = ("apple", "banana", "cherry")
 
-# thislist = ["apple", "banana", "cherry"]
-# for i in range(len(thislist)):
-#     print(thislist[i])
+# green, yellow, red = fruits
 
-# thislist = ["apple", "banana", "cherry"]
-# i = 0
-# while i < len(thislist):
-#     print(thislist[i])
-#     i = i + 1
+# print(green)
+# print(yellow)
+# print(red)
 
 
-# thislist = ["apple", "banana", "cherry"]
-# [print(x) for x in thislist]
+# fruits = ("apple", "banana", "cherry", "strawberry", "raspberry")
 
-# fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
-# newlist = []
+# green, yellow, *red = fruits
 
-# for x in fruits:
-#     if "a" in x:
-#         newlist.append(x)
-
-# print(newlist)
+# print(green)
+# print(yellow)
+# print(red)
 
 
-# fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+# fruits = ("apple", "mango", "papaya", "pineapple", "cherry")
 
-# newlist = [x for x in fruits if "a" in x]
-# print(newlist)
+# green, *tropic, red = fruits
 
-# newlist = [expression for item in iterable if condition == True]
-
-# thislist = ["orange", "mango", "kiwi", "pineapple", "banana"]
-# thislist.sort()
-# print(thislist)
-
-# thislist = [100, 50, 65, 82, 23]
-# thislist.sort()
-# print(thislist)
-# thislist = ["orange", "mango", "kiwi", "pineapple", "banana"]
-# thislist.sort(reverse=True)
-# print(thislist)
+# print(green)
+# print(tropic)
+# print(red)
 
 
-# def myfunc(n):
-#     return abs(n - 50)
+# tuple1 = ("a", "b", "c")
+# tuple2 = (1, 2, 3)
 
+# tuple3 = tuple1 + tuple2
+# print(tuple3)
 
-# thislist = [100, 50, 65, 82, 23]
-# thislist.sort(key=myfunc)
-# print(thislist)
+fruits = ("apple", "banana", "cherry")
+mytuple = fruits * 2
 
-
-# thislist = ["banana", "Orange", "Kiwi", "cherry"]
-# thislist.reverse()
-# print(thislist)
-
-
-# thislist = ["apple", "banana", "cherry"]
-# mylist = thislist.copy()
-# print(mylist)
-
-# thislist = ["apple", "banana", "cherry"]
-# mylist = list(thislist)
-# print(mylist)
-
-# thislist = ["apple", "banana", "cherry"]
-# mylist = thislist[:]
-# print(mylist)
-
-# list1 = ["a", "b", "c"]
-# list2 = [1, 2, 3]
-
-# list3 = list1 + list2
-# print(list3)
-
-# list1 = ["a", "b", "c"]
-# list2 = [1, 2, 3]
-
-# for x in list2:
-#     list1.append(x)
-
-# print(list1)
+print(mytuple)
