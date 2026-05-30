8f6fa8adf2f3695d161cfcdb4e392312e1513b6c
Ali Rahimi <ali.rahimi@gandom.co>
2026-05-30 12:23:03 +0200

    dicts

--- main.py
+++ main.py
@@ -1,157 +1,75 @@
-# thisset = {"apple", "banana", "cherry"}
-# print(thisset)
-
-# thisset = {"apple", "banana", "cherry", "apple"}
-
-# print(thisset)
-
-# thisset = {"apple", "banana", "cherry", True, 1, 2}
-
-# print(thisset)
-
-# thisset = {"apple", "banana", "cherry", False, True, 0}
-
-# print(thisset)
+# thisdict = {"brand": "Ford", "model": "Mustang", "year": 1964}
+# print(thisdict)
 
 
-# set1 = {"apple", "banana", "cherry"}
-# set2 = {1, 5, 7, 9, 3}
-# set3 = {True, False, False}
+# thisdict = {"brand": "Ford", "model": "Mustang", "year": 1964}
+# print(thisdict["brand"])
 
-# set1 = {"abc", 34, True, 40, "male"}
+# thisdict = {"brand": "Ford", "model": "Mustang", "year": 1964}
+# x = thisdict["model"]
 
-# print(set1)
-
-
-# thisset = {"apple", "banana", "cherry"}
-
-# for x in thisset:
-#     print(x)
-
-# thisset = {"apple", "banana", "cherry"}
-
-# print("banana" in thisset)
-
-# thisset = {"apple", "banana", "cherry"}
-
-# thisset.add("orange")
-
-# print(thisset)
-
-
-# thisset = {"apple", "banana", "cherry"}
-# tropical = {"pineapple", "mango", "papaya"}
-
-# thisset.update(tropical)
-
-# print(thisset)
-
-
-# thisset = {"apple", "banana", "cherry"}
-# mylist = ["kiwi", "orange"]
-
-# thisset.update(mylist)
-
-# print(thisset)
-
-# thisset = {"apple", "banana", "cherry"}
-
-# thisset.remove("banana")
-
-# print(thisset)
-
-# thisset = {"apple", "banana", "cherry"}
-
-# thisset.discard("banana")
-
-# print(thisset)
-
-# thisset = {"apple", "banana", "cherry"}
-
-# x = thisset.pop()
-
+# x = thisdict.get("model")
 # print(x)
 
-# print(thisset)
 
-# thisset = {"apple", "banana", "cherry"}
+# car = {"brand": "Ford", "model": "Mustang", "year": 1964}
 
-# thisset.clear()
+# x = car.keys()
 
-# print(thisset)
+# print(x)  # before the change
 
+# car["color"] = "white"
 
-# set1 = {"a", "b", "c"}
-# set2 = {1, 2, 3}
+# print(x)  # after the change
 
-# set3 = set1.union(set2)
-# print(set3)
+# car = {"brand": "Ford", "model": "Mustang", "year": 1964}
 
+# x = car.values()
 
-# set1 = {"a", "b", "c"}
-# set2 = {1, 2, 3}
+# print(x)  # before the change
 
-# set3 = set1 | set2
-# print(set3)
+# car["year"] = 2020
 
-# set1 = {"a", "b", "c"}
-# set2 = {1, 2, 3}
-# set3 = {"John", "Elena"}
-# set4 = {"apple", "bananas", "cherry"}
+# print(x)  # after the change
 
-# myset = set1.union(set2, set3, set4)
-# print(myset)
+# car = {"brand": "Ford", "model": "Mustang", "year": 1964}
 
+# x = car.values()
 
-# set1 = {"a", "b", "c"}
-# set2 = {1, 2, 3}
-# set3 = {"John", "Elena"}
-# set4 = {"apple", "bananas", "cherry"}
+# print(x)  # before the change
 
-# myset = set1 | set2 | set3 | set4
-# print(myset)
+# car["color"] = "red"
+
+# print(x)  # after the change
 
 
-# set1 = {"apple", "banana", "cherry"}
-# set2 = {"google", "microsoft", "apple"}
+# thisdict = {"brand": "Ford", "model": "Mustang", "year": 1964}
+# thisdict.update({"year": 2020})
+# print(thisdict)
 
-# set3 = set1.intersection(set2)
-# print(set3)
+# thisdict = {"brand": "Ford", "model": "Mustang", "year": 1964}
+# thisdict["color"] = "red"
+# print(thisdict)
 
-# set1 = {"apple", "banana", "cherry"}
-# set2 = {"google", "microsoft", "apple"}
+# thisdict = {"brand": "Ford", "model": "Mustang", "year": 1964}
+# thisdict.update({"color": "red"})
 
-# set3 = set1 & set2
-# print(set3)
+# thisdict = {"brand": "Ford", "model": "Mustang", "year": 1964}
+# thisdict.pop("model")
+# print(thisdict)
 
-# set1 = {"apple", "banana", "cherry"}
-# set2 = {"google", "microsoft", "apple"}
+# thisdict = {"brand": "Ford", "model": "Mustang", "year": 1964}
+# thisdict.popitem()
+# print(thisdict)
 
-# set1.intersection_update(set2)
+# thisdict = {"brand": "Ford", "model": "Mustang", "year": 1964}
+# del thisdict["model"]
+# print(thisdict)
 
-# print(set1)
+# thisdict = {"brand": "Ford", "model": "Mustang", "year": 1964}
+# thisdict.clear()
+# print(thisdict)
 
-
-# set1 = {"apple", "banana", "cherry"}
-# set2 = {"google", "microsoft", "apple"}
-
-# set3 = set1.difference(set2)
-
-# print(set3)
-
-# set1 = {"apple", "banana", "cherry"}
-# set2 = {"google", "microsoft", "apple"}
-
-# set3 = set1 - set2
-# print(set3)
-
-# set1 = {"apple", "banana", "cherry"}
-# set2 = {"google", "microsoft", "apple"}
-
-# set3 = set1.symmetric_difference(set2)
-
-# print(set3)
-
-x = frozenset({"apple", "banana", "cherry"})
-print(x)
-print(type(x))
+# thisdict = {"brand": "Ford", "model": "Mustang", "year": 1964}
+# mydict = thisdict.copy()
+# print(mydict)
