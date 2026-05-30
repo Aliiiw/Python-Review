9e8f86350afb937187691be4f14a7279cbe239e0
Ali Rahimi <ali.rahimi@gandom.co>
2026-05-30 14:55:17 +0200

    loops

--- main.py
+++ main.py
@@ -1,133 +1,26 @@
-# # # # # # # a = 33
-# # # # # # # b = 200
-# # # # # # # if b > a:
-# # # # # # #     print("b is greater than a")
+# i = 1
+# while i < 6:
+#     print(i)
+#     i += 1
 
 
-# # # # # # # age = 20
-# # # # # # # if age >= 18:
-# # # # # # #     print("You are an adult")
-# # # # # # #     print("You can vote")
-# # # # # # #     print("You have full legal rights")
+# i = 1
+# while i < 6:
+#     print(i)
+#     if i == 3:
+#         break
+#     i += 1
 
+# i = 0
+# while i < 6:
+#     i += 1
+#     if i == 3:
+#         continue
+#     print(i)
 
-# # # # # # a = 33
-# # # # # # b = 33
-# # # # # # if b > a:
-# # # # # #     print("b is greater than a")
-# # # # # # elif a == b:
-# # # # # #     print("a and b are equal")
-
-
-# # # # # score = 75
-
-# # # # # if score >= 90:
-# # # # #     print("Grade: A")
-# # # # # elif score >= 80:
-# # # # #     print("Grade: B")
-# # # # # elif score >= 70:
-# # # # #     print("Grade: C")
-# # # # # elif score >= 60:
-# # # # #     print("Grade: D")
-
-
-# # # # age = 25
-
-# # # # if age < 13:
-# # # #     print("You are a child")
-# # # # elif age < 20:
-# # # #     print("You are a teenager")
-# # # # elif age < 65:
-# # # #     print("You are an adult")
-# # # # elif age >= 65:
-# # # #     print("You are a senior")
-
-# # # day = 3
-
-# # # if day == 1:
-# # #     print("Monday")
-# # # elif day == 2:
-# # #     print("Tuesday")
-# # # elif day == 3:
-# # #     print("Wednesday")
-# # # elif day == 4:
-# # #     print("Thursday")
-# # # elif day == 5:
-# # #     print("Friday")
-# # # elif day == 6:
-# # #     print("Saturday")
-# # # elif day == 7:
-# # #     print("Sunday")
-
-# # a = 200
-# # b = 33
-# # if b > a:
-# #     print("b is greater than a")
-# # elif a == b:
-# #     print("a and b are equal")
-# # else:
-# #     print("a is greater than b")
-
-# # a = 200
-# # b = 33
-# # if b > a:
-# #     print("b is greater than a")
-# # else:
-# #     print("b is not greater than a")
-
-
-# # username = "Emil"
-# # password = "python123"
-# # is_active = True
-
-# # if username:
-# #     if password:
-# #         if is_active:
-# #             print("Login successful")
-# #         else:
-# #             print("Account is not active")
-# #     else:
-# #         print("Password required")
-# # else:
-# #     print("Username required")
-
-
-# # age = 16
-
-# # if age < 18:
-# #     pass  # TODO: Add underage logic later
-# # else:
-# #     print("Access granted")
-
-# # day = 4
-# # match day:
-# #     case 1:
-# #         print("Monday")
-# #     case 2:
-# #         print("Tuesday")
-# #     case 3:
-# #         print("Wednesday")
-# #     case 4:
-# #         print("Thursday")
-# #     case 5:
-# #         print("Friday")
-# #     case 6:
-# #         print("Saturday")
-# #     case 7:
-# #         print("Sunday")
-
-# # day = 4
-# # match day:
-# #     case 6:
-# #         print("Today is Saturday")
-# #     case 7:
-# #         print("Today is Sunday")
-# #     case _:
-# #         print("Looking forward to the Weekend")
-
-# day = 4
-# match day:
-#     case 1 | 2 | 3 | 4 | 5:
-#         print("Today is a weekday")
-#     case 6 | 7:
-#         print("I love weekends!")
+i = 1
+while i < 6:
+    print(i)
+    i += 1
+else:
+    print("i is no longer less than 6")
