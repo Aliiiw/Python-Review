97f6602e04d020ad653339c3ac30c36df7f4a4e0
Ali Rahimi <ali.rahimi@gandom.co>
2026-05-30 15:51:27 +0200

    math

--- main.py
+++ main.py
@@ -1,18 +1,32 @@
-import datetime
+import math
 
-# x = datetime.datetime.now()
+# x = min(5, 10, 25)
+# y = max(5, 10, 25)
+
+# print(x)
+# print(y)
+
+
+# x = abs(-7.25)
+
 # print(x)
 
 
-# x = datetime.datetime.now()
-
-# print(x.year)
-# print(x.strftime("%A"))
-
-# x = datetime.datetime(2020, 5, 17)
+# x = pow(4, 3)
 
 # print(x)
 
-x = datetime.datetime(2018, 6, 1)
 
-print(x.strftime("%B"))
+# x = math.sqrt(64)
+
+# print(x)
+
+# x = math.ceil(1.4)
+# y = math.floor(1.4)
+
+# print(x)  # returns 2
+# print(y)  # returns 1
+
+x = math.pi
+
+print(x)
