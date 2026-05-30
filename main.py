a87b1ddc7c5049448983aa08c2d366658da053b1
Ali Rahimi <ali.rahimi@gandom.co>
2026-05-30 15:42:13 +0200

    module

--- main.py
+++ main.py
@@ -1,59 +1,6 @@
-# mytuple = ("apple", "banana", "cherry")
-# myit = iter(mytuple)
+import mymodule
 
-# print(next(myit))
-# print(next(myit))
-# print(next(myit))
+mymodule.greeting("Ali")
 
-
-# mystr = "banana"
-# myit = iter(mystr)
-
-# print(next(myit))
-# print(next(myit))
-# print(next(myit))
-# print(next(myit))
-# print(next(myit))
-# print(next(myit))
-
-
-# class MyNumbers:
-#     def __iter__(self):
-#         self.a = 1
-#         return self
-
-#     def __next__(self):
-#         x = self.a
-#         self.a += 1
-#         return x
-
-
-# myclass = MyNumbers()
-# myiter = iter(myclass)
-
-# print(next(myiter))
-# print(next(myiter))
-# print(next(myiter))
-# print(next(myiter))
-# print(next(myiter))
-
-
-class MyNumbers:
-    def __iter__(self):
-        self.a = 1
-        return self
-
-    def __next__(self):
-        if self.a <= 20:
-            x = self.a
-            self.a += 1
-            return x
-        else:
-            raise StopIteration
-
-
-myclass = MyNumbers()
-myiter = iter(myclass)
-
-for x in myiter:
-    print(x)
+a = mymodule.person1["age"]
+print(a)
