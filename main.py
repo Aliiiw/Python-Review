bfd39afd7ced62f084f149669225878ba2d82c32
Ali Rahimi <ali.rahimi@gandom.co>
2026-06-03 16:13:38 +0200

    inner class

--- main.py
+++ main.py
@@ -1,118 +1,79 @@
-# # # class Person:
-# # #     def __init__(self, name, age):
-# # #         self.name = name
-# # #         self.__age = age  # Private property
+# # # class Outer:
+# # #     def __init__(self):
+# # #         self.name = "Outer Class"
+
+# # #     class Inner:
+# # #         def __init__(self):
+# # #             self.name = "Inner Class"
+
+# # #         def display(self):
+# # #             print("This is the inner class")
 
 
-# # # p1 = Person("Emil", 25)
-# # # print(p1.name)
-# # # print(p1.__age)  # This will cause an error
+# # # outer = Outer()
+# # # print(outer.name)
 
 
-# # # class Person:
-# # #     def __init__(self, name, age):
-# # #         self.name = name
-# # #         self.__age = age
+# # class Outer:
+# #     def __init__(self):
+# #         self.name = "Outer"
 
-# # #     def get_age(self):
-# # #         return self.__age
+# #     class Inner:
+# #         def __init__(self):
+# #             self.name = "Inner"
 
-
-# # # p1 = Person("Tobias", 25)
-# # # print(p1.get_age())
-
-
-# # class Person:
-# #     def __init__(self, name, age):
-# #         self.name = name
-# #         self.__age = age
-
-# #     def get_age(self):
-# #         return self.__age
-
-# #     def set_age(self, age):
-# #         if age > 0:
-# #             self.__age = age
-# #         else:
-# #             print("Age must be positive")
+# #         def display(self):
+# #             print("Hello from inner class")
 
 
-# # p1 = Person("Tobias", 25)
-# # print(p1.get_age())
-
-# # p1.set_age(26)
-# # print(p1.get_age())
+# # outer = Outer()
+# # inner = outer.Inner()
+# # inner.display()
 
 
-# # class Student:
-# #     def __init__(self, name):
-# #         self.name = name
-# #         self.__grade = 0
-
-# #     def set_grade(self, grade):
-# #         if 0 <= grade <= 100:
-# #             self.__grade = grade
-# #         else:
-# #             print("Grade must be between 0 and 100")
-
-# #     def get_grade(self):
-# #         return self.__grade
-
-# #     def get_status(self):
-# #         if self.__grade >= 60:
-# #             return "Passed"
-# #         else:
-# #             return "Failed"
-
-
-# # student = Student("Emil")
-# # student.set_grade(85)
-# # print(student.get_grade())
-# # print(student.get_status())
-
-
-# # class Person:
-# #     def __init__(self, name, salary):
-# #         self.name = name
-# #         self._salary = salary  # Protected property
-
-
-# # p1 = Person("Linus", 50000)
-# # print(p1.name)
-# # print(p1._salary)  # Can access, but shouldn't
-
-
-# class Calculator:
+# class Outer:
 #     def __init__(self):
-#         self.result = 0
+#         self.name = "Emil"
 
-#     def __validate(self, num):
-#         if not isinstance(num, (int, float)):
-#             return False
-#         return True
+#     class Inner:
+#         def __init__(self, outer):
+#             self.outer = outer
 
-#     def add(self, num):
-#         if self.__validate(num):
-#             self.result += num
-#         else:
-#             print("Invalid number")
+#         def display(self):
+#             print(f"Outer class name: {self.outer.name}")
 
 
-# calc = Calculator()
-# calc.add(10)
-# calc.add(5)
-# print(calc.result)
-# # calc.__validate(5) # This would cause an error
+# outer = Outer()
+# inner = outer.Inner(outer)
+# inner.display()
 
 
-class ScoreBoard:
-    def __init__(self, score):
-        self.__score = score
+class Car:
+    def __init__(self, brand, model):
+        self.brand = brand
+        self.model = model
+        self.engine = self.Engine()
 
-    def get_score(self):
-        return self.__score
+    class Engine:
+        def __init__(self):
+            self.status = "Off"
+
+        def start(self):
+            self.status = "Running"
+            print("Engine started")
+
+        def stop(self):
+            self.status = "Off"
+            print("Engine stopped")
+
+    def drive(self):
+        if self.engine.status == "Running":
+            print(f"Driving the {self.brand} {self.model}")
+        else:
+            print("Start the engine first!")
 
 
-s1 = ScoreBoard(0)
-
-print(s1.get_score())
+car = Car("Toyota", "Corolla")
+car.drive()
+car.engine.start()
+car.drive()
